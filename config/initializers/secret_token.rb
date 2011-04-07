6d426625f5f28d782fa552df10a2b07f302b1252
Allen Redding <dev@leathertank.com>
2011-04-06 21:46:59 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '8630d09b34cfb6dade04b58e6cafded15d640936ebc9560f3aed04eb3e812333b443a0c74b4c20629be1cc27679af782c06a8bdf872669e693c826364891b7f1'
